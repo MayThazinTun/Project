27aaaac1e815fac9728172c4d8f12461e9249594
MayThazinTun <maythazin.tun1972001@gmail.com>
2024-06-03 16:08:04 +0630

    first commit

---
+++ Layout/nav.php
@@ -0,0 +1,29 @@
+
+<nav class="d-flex justify-content-between py-3 px-5 border border-bottom-2">
+        <div class="d-flex align-item-center">
+            <img src="./images/Logo.png" class="rounded me-3" style="width: 50px;height: 50px;">
+            <h3><a href="" style="text-decoration:none;" class="text-dark">Tee World Myanmar</a></h3>
+        </div>
+        <div>
+            <ul class="nav me-4">
+                <li class="nav-item">
+                    <a href="" class="nav-link text-dark">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link text-dark">T-shirts</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link text-dark">Store</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link text-dark">Cart</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link text-dark">History</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link text-dark">My account</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php require_once("./Layout/header.php") ?>
+
+    <div class="row" id="contact">
+        <div class="col-7">
+            <img src="" id="img">
+        </div>
+        <div class="col-5 text-white" id="card">
+            <h2>Welcome</h2>
+            <p class="">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quam illum! Eaque tempora omnis
+                excepturi nihil quas enim libero facilis, corrupti error molestias quo pariatur repudiandae numquam?
+                Asperiores, sit nihil.
+            </p>
+            <div class="btn border border-1-dark" id="btn">Shop now   <i class="fa-solid fa-arrow-right"></i>   </div>    
+        </div>
+    </div>
+
+<?php require_once("./Layout/footer.php") ?>
